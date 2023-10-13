c4aa7b17f9ea07ffe6d871dfddaa29e11adeb2d6
Zdravkson <jovan.zdravkovic882@gmail.com>
2023-10-14 00:16:05 +0200

    Added code and input.

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define MAX_N 1000 // MAKSIMALAN BROJ KOORDINATA
+
+using namespace std;
+
+double coefficientMatrix[MAX_N][MAX_N + 1];
+double ans[MAX_N];
+pair<double, double> coordinates[MAX_N];
+
+int main() {
+    int n;
+    cout << "Unesite broj koordinata: ";
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        cout << "Unesite x koordinatu " << i + 1 << ". tacke: ";
+        cin >> coordinates[i].first;
+        cout << "Unesite y koordinatu " << i + 1 << ". tacke: ";
+        cin >> coordinates[i].second;
+    }
+    return 0;
+}
