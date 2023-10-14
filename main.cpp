ac79e09c7730a2be568e8665b0395f13ba8c1db4
Zdravkson <jovan.zdravkovic882@gmail.com>
2023-10-14 10:42:45 +0200

    Added filling the coefficient matrix

--- main.cpp
+++ main.cpp
@@ -18,5 +18,13 @@ int main() {
         cin >> coordinates[i].second;
     }
     // popunjavamo matricu sa koeficijentima jednacina sistema sa n jednacina i n nepoznatih
+    for(int i = 0; i < n; i++) {
+        coefficientMatrix[i][n] = coordinates[i].second;
+        double exponentation = 1;
+        for(int j = n - 1; j >= 0; j--) {
+            coefficientMatrix[i][j] = exponentation;
+            exponentation *= coordinates[i].first;
+        }
+    }
     return 0;
 }
